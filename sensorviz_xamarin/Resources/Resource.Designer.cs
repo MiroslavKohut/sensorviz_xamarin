b6c4d2684fb91d9cd45978e8b1d1103b9914ba60
MichalDobis <miso.dobis@gmail.com>
2017-03-16 15:17:07 +0100

    layout update

--- sensorviz_xamarin/Resources/Resource.Designer.cs
+++ sensorviz_xamarin/Resources/Resource.Designer.cs
@@ -41,6 +41,22 @@ namespace sensorviz_xamarin
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int biely_layout = 2130968576;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -60,14 +76,14 @@ namespace sensorviz_xamarin
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int relativeLayout1 = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int relativeLayout1 = 2131099648;
 			
-			// aapt resource value: 0x7f050002
-			public const int textView1 = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int textView1 = 2131099650;
 			
-			// aapt resource value: 0x7f050001
-			public const int textureView1 = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int textureView1 = 2131099649;
 			
 			static Id()
 			{
@@ -98,11 +114,11 @@ namespace sensorviz_xamarin
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
 			static String()
 			{
