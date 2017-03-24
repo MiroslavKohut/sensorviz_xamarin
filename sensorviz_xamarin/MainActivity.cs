6124feba880e6de523c220ab0aa8e39fffc910ca
MichalDobis <miso.dobis@gmail.com>
2017-03-24 22:32:45 +0100

    added proximity sensor

--- sensorviz_xamarin/MainActivity.cs
+++ sensorviz_xamarin/MainActivity.cs
@@ -4,8 +4,12 @@ using Android.App;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Android.Hardware.Camera2;
 using Android.Hardware;
 using Android.Content.PM;
+//using Android.Graphics;
+
+
 
 using Android.Content;
 using Android.Runtime;
@@ -15,10 +19,15 @@ namespace TextureViewCameraStream
 {
     [Activity(Label = "TextureViewCameraStream", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
     ScreenOrientation = ScreenOrientation.Landscape)]
-    public class Activity1 : Activity, TextureView.ISurfaceTextureListener
+    public class Activity1 : Activity, TextureView.ISurfaceTextureListener, Android.Hardware.ISensorEventListener
     {
         Camera camera;
         TextureView textureView;
+        FrameLayout mainLayout;
+
+        static readonly object _syncLock = new object();
+        TextView proximityTextView;
+        LinearLayout proximityLayout;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -29,7 +38,14 @@ namespace TextureViewCameraStream
 
             camera = Camera.Open();
 
-           // _textureView.LayoutParameters = new FrameLayout.LayoutParams(w, h);
+            var sensorService = (SensorManager)GetSystemService(Context.SensorService);
+
+            // Get a Light Sensor
+            var lightSensor = sensorService.GetDefaultSensor(SensorType.Light);
+
+            // Register this class a listener for light sensor
+            sensorService.RegisterListener(this, lightSensor, Android.Hardware.SensorDelay.Game);
+
 
             try
             {
@@ -45,6 +61,19 @@ namespace TextureViewCameraStream
             SetContentView(sensorviz_xamarin.Resource.Layout.Main);
             textureView = FindViewById<TextureView>(sensorviz_xamarin.Resource.Id.textureView1);
             textureView.SurfaceTextureListener = this;
+           
+            //add proximity layout
+            proximityLayout = FindViewById<LinearLayout>(sensorviz_xamarin.Resource.Id.proximity);
+
+            proximityTextView = new TextView(proximityLayout.Context);
+            proximityTextView.Text = "proximity";
+            proximityTextView.SetBackgroundColor(Android.Graphics.Color.Yellow);
+            proximityTextView.SetTextColor(Android.Graphics.Color.DarkGray);
+
+            proximityLayout.AddView(proximityTextView);
+
+
+            mainLayout = FindViewById<FrameLayout>(sensorviz_xamarin.Resource.Id.mainLayout1);
         }
 
         public void OnSurfaceTextureAvailable(Android.Graphics.SurfaceTexture surface, int w, int h)
@@ -82,6 +111,24 @@ namespace TextureViewCameraStream
         {
 
         }
+
+
+        public void OnSensorChanged(SensorEvent s)
+        {
+            lock (_syncLock)
+            {
+                proximityTextView.Text = string.Format("Brightness={0:f}", s.Values[0]);
+              
+                proximityLayout.Alpha = (float)0.05 * s.Values[0];
+                
+            }
+        }
+
+        public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
+        {
+            // Your processing here
+        }
+
 
     }
 }
